78ad354eba6cc44ecbb92da9c986249fae1c15bf
voycik <voycik@gmail.com>
2018-12-01 22:00:04 +0100

    complete article feature

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+RSpec.feature "Creating Articles" do
+  scenario "A user creates a new article" do
+    visit "/"
+    click_link "New article"
+    fill_in "Title", with: "First article"
+    fill_in "Body", with: "Lorem ipsum"
+    click_button "Create Article"
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
+  end
+end
