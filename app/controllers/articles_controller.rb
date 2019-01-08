6b3bb5796a51a0515a6bea8ff1d74856bc745b0b
voycik <voycik@gmail.com>
2019-01-08 22:43:50 +0100

    Restrict edit only for articles owners

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,16 +31,26 @@ class ArticlesController < ApplicationController
   end
 
 
-  def edit; end
+  def edit
+    unless @article.user == current_user
+      flash[:alert] = "You can only edit your own article."
+      redirect_to article_path
+    end
+  end
 
   def update
 
-    if @article.update(article_params)
-      flash[:succes] = 'Article has been updated'
-      redirect_to @article
+    unless @article.user == current_user
+      flash[:danger] = "You can only edit your own article."
+      redirect_to article_path
     else
-      flash.now[:danger] = 'Article has not been updated'
-      render :edit
+      if @article.update(article_params)
+        flash[:succes] = 'Article has been updated'
+        redirect_to @article
+      else
+        flash.now[:danger] = 'Article has not been updated'
+        render :edit
+      end
     end
   end
 
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 RSpec.describe 'Articles', type: :request do
   before do
-    @article = Article.create(title: 'Title one', body: 'Body of article one')
+    @john = User.create(email: 'john@rambo.com', password: 'qwerty')
+    @fred = User.create(email: 'fred@durst.com', password: 'rolling')
+    @article = Article.create!(title: 'Title one', body: 'Body of article one', user: @john)
   end
 
   describe 'GET /articles/:id' do
@@ -19,6 +21,38 @@ RSpec.describe 'Articles', type: :request do
         expect(response.status).to eq 302
         flash_message = 'The article you are looking for could not be found'
         expect(flash[:alert]).to eq flash_message
+      end
+    end
+  end
+
+  describe 'GET /articles/:id/edit' do
+    context 'with non-signed in user' do
+      before {get "/articles/#{@article.id}/edit"}
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+    context 'with signed in users who are non-owners' do
+      before do
+        login_as(@fred)
+        get "/articles/#{@article.id}/edit"
+      end
+      it "redirects to the show article page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with signed in users who are owners' do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+      it "succefully edits article" do
+        expect(response.status).to eq 200
       end
     end
   end
